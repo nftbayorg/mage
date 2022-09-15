6df68f124d03675a291148337c1d20c1faf92452
Dean Selvey <deanselvey@gmail.com>
2022-09-15 10:45:17 +0100

    file upload amendments

--- src/components/CreateItem.tsx
+++ src/components/CreateItem.tsx
@@ -2,9 +2,9 @@ import { useForm } from "react-hook-form";
 import Input from "../components/form/Input";
 import TextArea from "../components/form/TextArea";
 import { DropZone } from "../components/form/DropZone";
-import { FileList } from "../components/form/FileList";
-import { useCallback, useRef, useState } from "react";
+import { MouseEventHandler, useCallback, useRef, useState } from "react";
 import { FaImage, FaAsterisk, FaTimes } from "react-icons/fa";
+import Image from "next/image";
 
 interface FilePreview {
   url: string;
@@ -34,21 +34,29 @@ const CreateItem = () => {
     }
   }, [])
 
-  const removeFile = () => {
+  const removeFile: MouseEventHandler<HTMLDivElement> = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     if (file) {
       URL.revokeObjectURL(file?.url);
+    }
+
+    if (inputRef && inputRef.current) {
+      inputRef.current.value = '';
     }
 
     setFile(undefined);
   }
 
-  const onFileSelected = (event: any) => {
+  const onFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.stopPropagation();
     event.preventDefault();
-    const file = event?.target?.files[0];
 
-    if (file) {
-      setFile({ url: URL.createObjectURL(file), file: file })
+    if (event.target.files) {
+      const file = event?.target?.files[0];
+      if (file) {
+        setFile({ url: URL.createObjectURL(file), file: file })
+      }
     }
   }
 
@@ -59,7 +67,7 @@ const CreateItem = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <>
       <div className="flex items-center my-1">
         <FaAsterisk className="fill-red-500 mr-2" size={10}/>
         <label className="text-sm text-gray-400">Required fields</label>
@@ -73,53 +81,53 @@ const CreateItem = () => {
         onDragStateChange={onDragStateChange}
         onFilesDrop={onFilesDrop}
       >
-        {!file && 
-          <div className="flex items-center justify-center p-2 w-full h-96 md:w-96 border-4 border-dashed rounded-lg">
-            <button onClick={onOpenFileDialog} className="flex items-center justify-center bg-gray-300 w-full h-full rounded-lg">
-              <input type="file" id="file" ref={inputRef} className="hidden" onChange={onFileSelected}/>
-              <FaImage className="fill-gray-700" size={80}/>
-            </button>
-          </div>
-        }
-
-        {file && 
-          <div className="rounded-lg border-4 border-dashed p-2 flex items-center justify-center h-auto w-auto relative">
-            <button className="absolute top-3 right-3" onClick={removeFile}>
-              <FaTimes className="fill-slate-400" />
-            </button>
-            <div className="h-auto w-auto p-8">
-              <img width={600} height={600} className="rounded block max-w-sm max-h-sm md:max-w-lg md:max-h-lg w-auto h-auto" src={file.url}/>
+        <button onClick={onOpenFileDialog} className="flex items-center justify-center p-2 w-full h-96 md:w-96 border-4 border-dashed rounded-lg">
+          <input type="file" id="file" ref={inputRef} className="hidden" onChange={onFileSelected}/>
+          {file ?
+            <div className="w-full h-full relative">
+              <div role="button" className="absolute top-3 right-3 z-10" onClick={removeFile}>
+                <FaTimes className="fill-slate-400" />
+              </div>
+              <Image alt="image" objectFit="cover" layout="fill" className="rounded-lg block max-w-sm max-h-sm md:max-w-lg md:max-h-lg w-auto h-auto p-[inherit] p-2" src={file.url}/>
             </div>
-          </div>
-        }
+            : 
+            <div className="flex items-center justify-center p-2 bg-gray-300 w-full h-full rounded-lg">
+              <FaImage className="fill-gray-700" size={80}/>
+            </div>
+          }
+        </button>
+
       </DropZone>      
 
-      <Input
-        required
-        label="Name"
-        placeholder="Item name"
-        {...register("name", { required: true })}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
 
-      <Input
-        label="External link"
-        placeholder="https://yoursite.io/item/123"
-        caption="Mage will include a link on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details."
-        {...register("link")}
-      />
+        <Input
+          required
+          label="Name"
+          placeholder="Item name"
+          {...register("name", { required: true })}
+        />
 
-      <TextArea        
-        label="Description"
-        placeholder="Provide a detailed description of your item."
-        caption="The description will be included on your item's detail page underneath its image."
-        {...register("link")}
-      />
+        <Input
+          label="External link"
+          placeholder="https://yoursite.io/item/123"
+          caption="Mage will include a link on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details."
+          {...register("link")}
+        />
 
-      <button className="flex items-center justify-center space-x-4 py-4 px-10 hover:bg-blue-400 bg-blue-500 rounded font-semibold">
-        <div className="text-white">Create Item</div>
-      </button>
-      {/* {errors.name && <span>This field is required</span>} */}
-    </form>
+        <TextArea        
+          label="Description"
+          placeholder="Provide a detailed description of your item."
+          caption="The description will be included on your item's detail page underneath its image."
+          {...register("link")}
+        />
+
+        <button className="flex items-center justify-center space-x-4 py-4 px-10 hover:bg-blue-400 bg-blue-500 rounded font-semibold">
+          <div className="text-white">Create Item</div>
+        </button>
+        {/* {errors.name && <span>This field is required</span>} */}
+      </form>
+    </>
   );
 };
 
