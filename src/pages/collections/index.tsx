9b486c261144da7859e82a2a11c6b96c17f2bf3e
Dean Selvey <deanselvey@gmail.com>
2022-09-21 14:22:15 +0100

    split out component panel

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,6 +1,20 @@
 import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import { getMageAuthSession } from "../../server/common/get-server-session";
-import { trpc } from "../../utils/trpc";
+import { trpc, inferQueryOutput } from "../../utils/trpc";
+
+type Collection = inferQueryOutput<"collection.get">;
+type CollectionProps = {
+  collection: Collection
+}
+
+const CollectionPanel = ({ collection }: CollectionProps) => {
+
+  if (!collection) return <div>Unknown collection</div>
+
+  return (
+    <div>{collection.name}</div>
+  )
+}
 
 const Collections: NextPage<AuthenticatedPageProps> = ({ session }) => {
 
@@ -12,7 +26,9 @@ const Collections: NextPage<AuthenticatedPageProps> = ({ session }) => {
     <div className="p-5 mb-10 flex items-center justify-center w-full h-full overflow-y-scroll">
       <div className="md:p-4 text-2xl flex flex-col h-screen text-gray-700 font-medium dark:text-gray-300">
         <h1 className="text-4xl my-5">My Collections</h1>
-        {collections.data?.map(collection => (<div key={collection.id}>{collection.name}</div>))}
+        <div className="flex flex-col md:flex-row">
+          {collections.data?.map(collection => (<CollectionPanel key={collection.id} collection={collection}/>))}
+        </div>
       </div>
     </div>
   )
