ec8dfb93467d62b492a73f2a37bda69c96e1b201
Dean Selvey <deanselvey@gmail.com>
2022-10-03 12:57:46 +0100

    mobile nav bar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,89 @@
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-const NavBar = () => {
-  const { data } = useSession();
-  const router = useRouter();
+const MobileNav = () => {
+  const [active, setActive] = useState(false);  
+
+  function handleClick() {
+    setActive(currentActive => !currentActive);
+  }
 
   return (
+    <>
+      <div className="relative md:hidden">
+        {!active && <FaBars size={30} className="fill-gray-700" onClick={handleClick}/>}
+        {active && <FaTimes size={30} className="fill-gray-700" onClick={handleClick}/>}
+        <div className={`${!active ? "opacity-0": "opacity-100"} transition-opacity ease-in-out delay-150 border h-60 w-72 bg-white absolute md:relative top-10 right-1 rounded-lg shadow-lg p-10`}>
+          {<MenuItems/>}
+        </div>
+      </div>
+      <div className="hidden md:flex space-x-6 justify-center items-center w-full">
+        <MenuItems/>
+      </div>
+    </>
+  )
+}
+
+const MenuItems = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col gap-y-8 md:flex-row md:items-center md:justify-center w-full">
+      <div className="flex flex-col gap-y-4 md:flex-row md:space-x-6 md:items-center md:justify-center w-full">
+        <Link href="/trade">
+          <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
+            Trade
+          </a>
+        </Link>
+        {session && <Link href="/nfts/create">
+          <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
+            Create
+          </a>
+        </Link>}
+        {session && <Link href="/collections">
+          <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
+            My Collections
+          </a>
+        </Link>}
+      </div>
+      <div className="flex flex-col md:flex-row md:ml-auto min-w-fit">
+        {session && (
+          <button
+            className="md:block hover:bg-blue-400 p-3 px-6 pt-2 text-white bg-blue-500 rounded"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        )}
+        {!session && (
+          <button
+            className="hover:text-blue-500 p-3 px-6 pt-2"
+            onClick={() => router.push("/login")}
+          >
+            Log In
+          </button>
+        )}
+        {!session && (
+          <button
+            className="md:block hover:bg-blue-400  p-3 px-6 pt-2 text-white bg-blue-500 rounded"
+            onClick={() => router.push("/login")}
+          >
+            Register
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const NavBar = () => {
+  return (
     <nav className="relative container mx-auto p-6 border-b border-gray-200 dark:border-gray-600">
+      
       <div className="flex items-center justify-between">
         <Link href="/" passHref>
           <div className="flex items-center">
@@ -16,54 +92,7 @@ const NavBar = () => {
             </span>
           </div>
         </Link>
-        <div className="hidden md:flex space-x-6">
-          <Link href="/trade">
-            <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
-              Trade
-            </a>
-          </Link>
-          {/* <Link href="/stake">
-            <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
-              Earn
-            </a>
-          </Link> */}
-          {data && <Link href="/nfts/create">
-            <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
-              Create
-            </a>
-          </Link>}
-          {data && <Link href="/collections">
-            <a className="dark:text-gray-300 dark:hover:text-blue-500 hover:text-blue-500 text-gray-700">
-              My Collections
-            </a>
-          </Link>}
-        </div>
-        <div className="hidden md:flex">
-          {data && (
-            <button
-              className="hidden md:block hover:bg-blue-400 p-3 px-6 pt-2 text-white bg-blue-500 rounded"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </button>
-          )}
-          {!data && (
-            <button
-              className="hover:text-blue-500  p-3 px-6 pt-2"
-              onClick={() => router.push("/login")}
-            >
-              Log In
-            </button>
-          )}
-          {!data && (
-            <button
-              className="hidden md:block hover:bg-blue-400  p-3 px-6 pt-2 text-white bg-blue-500 rounded"
-              onClick={() => router.push("/login")}
-            >
-              Register
-            </button>
-          )}
-        </div>
+        <MobileNav />
       </div>
     </nav>
   );
--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -50,8 +50,8 @@ const CollectionsPage: NextPage<AuthenticatedPageProps> = ({ session }) => {
   return (
     <div className="p-5 mb-10 flex items-center justify-center h-full overflow-y-scroll overflow-x-hidden">
       <div className="md:p-4 text-2xl flex flex-col w-full h-screen text-gray-700 font-medium dark:text-gray-300 md:w-4/5">
-        <h1 className="text-5xl my-5">My Collections</h1>
-        <h2 className="text-md font-light">Create, curate, and manage collections of unique NFTs to share and sell.</h2>
+        <h1 className="text-3xl md:text-5xl my-5">My Collections</h1>
+        <h2 className="md:text-md font-light">Create, curate, and manage collections of unique NFTs to share and sell.</h2>
         <Link href="/collections/create">
           <a className="md:max-w-fit my-10 flex items-center justify-center dark:text-gray-300 py-4 px-10 hover:bg-blue-400 bg-blue-500 rounded font-semibold disabled:bg-blue-200 text-white">
             Create a collection
