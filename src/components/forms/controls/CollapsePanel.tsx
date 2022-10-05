4e8459a185b219a4f4805512bcc1ac0d4a74dd89
Dean Selvey <deanselvey@gmail.com>
2022-10-05 20:58:22 +0100

    click on panel header to expand

--- src/components/forms/controls/CollapsePanel.tsx
+++ src/components/forms/controls/CollapsePanel.tsx
@@ -15,16 +15,19 @@ export const CollapsePanel = ({ children, collapsible, defaultState, icon, label
 
   return (
     <section>
-      <div className={`${panelCollapsed ? "rounded-b-xl" : ""} flex p-7 border border-gray-200 dark:border-gray-600 rounded-t-xl items-center`}>
+      <div 
+        onClick={() => setPanelCollapsed(prev => !prev)} 
+        className={`${panelCollapsed ? "rounded-b-xl" : ""} cursor-pointer flex p-7 border border-gray-200 dark:border-gray-600 rounded-t-xl items-center`}
+      >
         {icon}
         <div className="ml-5 text-xl font-semibold text-gray-700">
           {label}
         </div>
         {collapsible && 
-          <button className="ml-auto" onClick={() => setPanelCollapsed(prev => !prev)}>
+          <div className="ml-auto">
             {panelCollapsed && <FaChevronDown size={25} className="fill-gray-700 dark:fill-gray-400 font-light" />}
             {!panelCollapsed && <FaChevronUp size={25} className="fill-gray-700 dark:fill-gray-400 font-light" />}
-          </button>
+          </div>
         }
       </div>
       <div className={`${panelCollapsed ? "max-h-0 border-0" : "max-h-screen p-10"} transition-[height] ease-in-out delay-100 flex-col border border-t-0 border-gray-200 dark:border-gray-600 rounded-b-xl items-start justify-start bg-gray-50 overflow-hidden`}>
