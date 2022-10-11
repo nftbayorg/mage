bfceb95bb68e76de3173836ace9c75fd76e533ab
Dean Selvey <deanselvey@gmail.com>
2022-10-11 11:24:37 +0100

    stopped image flash on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,28 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { CollectionPanel } from "../components/views/collections/CollectionPanel";
-import { useMemo } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Home: NextPage = () => {
+  const [sorted, setSorted] = useState(false);
   const collections = trpc.collection.getAll.useQuery(undefined, {
     refetchOnWindowFocus: false,
   });
 
-  let { data } = collections;
+  const dataRef = useRef(collections.data);
 
-  if (!data) {
-    data = Array(6);
-  } else {
-    data.sort(() => Math.random() - Math.random());
+  if (!dataRef.current) {
+    dataRef.current = Array(6);
   }
+
+  useEffect(() => {
+    if (dataRef.current) {
+      dataRef.current.sort(() => Math.random() - Math.random());
+      setSorted(true);
+    }
+  }, [])
+
+  if (!sorted) <></>
 
   return (
     <div className="my-10">
@@ -32,7 +40,7 @@ const Home: NextPage = () => {
         <div className="flex flex-col items-start justify-center w-full pt-10 md:pt-6 text-2xl">
           <div className="text-3xl md:text-4xl text-gr">New and noteable</div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-h-96 gap-x-2 gap-y-10 justify-center w-full pt-6 text-2xl overflow-hidden">
-            {[data[0], data[1], data[2]].map((collection, idx) => {
+            {[dataRef.current[0], dataRef.current[1], dataRef.current[2]].map((collection, idx) => {
               return <CollectionPanel key={idx} collection={collection} />;
             })}
           </div>
@@ -40,7 +48,7 @@ const Home: NextPage = () => {
         <div className="flex flex-col items-start justify-center w-full md:pt-6 text-2xl">
           <div className="text-3xl md:text-4xl">Collectibles</div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-h-96 gap-x-2 gap-y-10 justify-center w-full pt-6 text-2xl overflow-hidden">
-            {[data[3], data[4], data[5]].map((collection, idx) => {
+            {[dataRef.current[3], dataRef.current[4], dataRef.current[5]].map((collection, idx) => {
               return <CollectionPanel key={idx} collection={collection} />;
             })}
           </div>
