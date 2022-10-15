8a22a1c80dd5713aa31a4abc2f2365c3dcd0c31d
Dean Selvey <deanselvey@gmail.com>
2022-10-15 21:14:34 +0100

    resolved build issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
 import Layout from "../components/layout/Layout";
 import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session }>) => {
 
   return (
     <SessionProvider session={pageProps.session}>
