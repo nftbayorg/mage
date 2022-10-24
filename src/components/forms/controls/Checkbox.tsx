37f82721f69ca65a0f29998f5bd2e59810cda4b9
Dean Selvey <deanselvey@gmail.com>
2022-10-24 14:58:50 +0100

    checkbox updates

--- src/components/forms/controls/Checkbox.tsx
+++ src/components/forms/controls/Checkbox.tsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const Checkbox = () => {
+type CheckBoxProps = {
+  onClick?: () => void;
+  initialValue: boolean;
+}
+
+export const Checkbox = ({ onClick, initialValue }: CheckBoxProps) => {
+
+  const [checked, setChecked] = useState(initialValue);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setChecked(initialValue);
+
+    if (inputRef.current) {
+      inputRef.current.checked = initialValue;
+    }
+  }, [initialValue]);
+
+  const handleClick = () => {
+    setChecked(prev => !prev);
+
+    if (onClick) onClick();
+  }
 
   return (
     <div className="">  
-      <div className="flex items-center mr-4 px-1">  
-        <input type="checkbox" id="A3-yes" name="A3-confirmation" className="opacity-0 absolute h-8 w-8" />  
-        <div className="border-2 rounded-md border-gray-500 dark:border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">  
-          <svg className="fill-current hidden w-3 h-3 text-blue-500 pointer-events-none" version="1.1" viewBox="0 0 17 12" xmlns="http://www.w3.org/2000/svg">  
+      <div className="flex items-center mr-4 px-1" onClick={handleClick}>  
+        <input ref={inputRef} type="checkbox" id="A3-yes" name="A3-confirmation" className="opacity-0 absolute h-8 w-8" />  
+        <div className={`border-2 rounded-md  ${checked ? 'border-blue-500 dark:border-blue-500' : 'border-gray-500 dark:border-gray-400'} w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500`}>  
+          <svg className="fill-current hidden bg-blue-500 w-5 h-5 p-1 text-white pointer-events-none" version="1.1" viewBox="0 0 17 12" xmlns="http://www.w3.org/2000/svg">  
             <g fill="none" fillRule="evenodd">  
-              <g transform="translate(-9 -11)" fill="#1F73F1" fillRule="nonzero">  
+              <g transform="translate(-9 -11)" fill="#ffffff" fillRule="nonzero">  
                 <path d="m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z" />  
               </g>  
             </g>  
--- src/components/views/collections/CollectionMenu.tsx
+++ src/components/views/collections/CollectionMenu.tsx
@@ -1,15 +1,37 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Checkbox } from "../../forms/controls/Checkbox";
 import { Menu, MenuGroup, MenuItem } from "../../layout/Menu"
 
 type CollectionMenuProps = {
   collapsed: boolean;
   collectionProperties: CollectionNftSetProperties | null;
+  onFilterSelection?: ({ properties }: { properties: CollectionNftSetProperty[] }) => void;
 }
 
 export const CollectionMenu = ({ collapsed, collectionProperties }: CollectionMenuProps) => {
 
   const [properties, setProperties] = useState(collectionProperties);
+
+  const [selectedProperties, setSelectedProperties] = useState<CollectionNftSetProperty[]>([]);
+
+  const handleClick = useCallback((clickedProperty) => {
+    const findPredicate = prop => prop.name === clickedProperty.name && prop.type === clickedProperty.type;
+    const filterPredicate = prop => prop.name !== clickedProperty.name && prop.type !== clickedProperty.type;
+
+    if (selectedProperties.find(findPredicate)) {
+      setSelectedProperties(prev => prev.filter(filterPredicate));
+    } else {
+      setSelectedProperties(prev => ([...prev, clickedProperty]));
+    }
+  }, [selectedProperties]);
+
+  const determineCheckedState = useCallback(value => {
+    if (selectedProperties && selectedProperties.find(prop => prop.name === value.name && prop.type === value.type)) {
+      return true;
+    }
+
+    return false;
+  }, [selectedProperties])
 
   return (
     <Menu classesName={`
@@ -32,15 +54,13 @@ export const CollectionMenu = ({ collapsed, collectionProperties }: CollectionMe
           >
             <>
               {properties.propertyCounts && properties.propertyCounts[propertyKey]?.map(value => {
-
                 if (value) {
-
                   return (
-                    <MenuItem key={value.name}>
+                    <MenuItem key={value.name} onClick={() => handleClick(value)}>
                       <div className="flex items-center font-medium text-ellipsis whitespace-nowrap overflow-hidden w-full">
                         <div>{value.name}</div>
                         <div className="ml-auto px-3 text-sm text-gray-700 dark:text-gray-500">{value._count}</div>
-                        <Checkbox />
+                        <Checkbox initialValue={determineCheckedState(value)}/>
                       </div>
                     </MenuItem>
                   )
