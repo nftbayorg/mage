0c8e3df639847917bfdad34c19806aac1e471b12
Dean Selvey <deanselvey@gmail.com>
2022-11-18 13:48:43 +0000

    focus background color for button

--- src/components/forms/controls/Button.tsx
+++ src/components/forms/controls/Button.tsx
@@ -10,13 +10,16 @@ export const Button = ({ caption, icon, onClick }: ButtonProps) => {
       onClick={onClick}
       className="flex items-center justify-center space-x-4 my-5 py-4 px-10 w-full md:w-fit
         bg-white rounded font-semibold 
+        focus:bg-white
         disabled:bg-blue-200
         border
         border-gray-300
         dark:border-gray-300
         dark:bg-gray-800
+        dark:focus:bg-gray-800
         dark:hover:bg-opacity-10
         dark:disabled:bg-opacity-0
+
         text-gray-800
         dark:text-white
         dark:disabled:text-gray-900
