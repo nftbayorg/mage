dd9180e397fc4383a51a9836828977185934e164
Dean Selvey <deanselvey@gmail.com>
2022-11-23 09:47:14 +0000

    margin adjustment

--- src/pages/collections/explore.tsx
+++ src/pages/collections/explore.tsx
@@ -10,7 +10,7 @@ const ExploreCollectionsPage: NextPage<AuthenticatedPageProps> = ({ collections 
   return (
     <div className="px-4 py-5 md:px-5 mb-10 flex items-center justify-center h-full overflow-y-scroll overflow-x-hidden">
       <div className="md:p-4 text-2xl flex flex-col w-full h-screen text-gray-700 font-medium dark:text-gray-200 md:w-11/12">
-        <h1 className="text-3xl md:text-5xl my-16">Explore Collections</h1>
+        <h1 className="text-3xl md:text-5xl my-5 md:my-10">Explore Collections</h1>
         {data && data.length && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
             {data.map((collection) => (
